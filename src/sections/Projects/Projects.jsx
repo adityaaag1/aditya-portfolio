42c9d23d6fbff346ff02a449ab854bc1370bcb0c
adityaaag1 <130077432+adityaaag1@users.noreply.github.com>
2024-08-06 16:23:08 +0530

    added files

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,28 @@
+import styles from './ProjectsStyles.module.css';
+import tasks from '../../assets/images.jpeg';
+import identity from '../../assets/pngtree-vector-id-icon-png-image_1024864.jpg';
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={tasks}
+          link="#"
+          h3="Task Scheduler using DAG"
+
+        />
+        <ProjectCard
+          src={identity}  
+          link="#"
+          h3="DIMS"
+      
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
